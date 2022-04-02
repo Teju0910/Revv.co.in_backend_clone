53f3f659991fab297b9cc981adbfb6c5f55c5797
Teju0910 <tejasvini_fw15_007@masai.school>
2022-04-02 23:19:39 +0530

    port added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require("./index");
 const connect = require("./configs/db");
-const PORT =  5656;
-app.listen(PORT, async () => {
+const port = process.env.PORT || 5656;
+app.listen(port, async () => {
     try{
         await connect();
-        console.log(`listening on port ${PORT}`)
+        console.log(`listening on port ${port}`)
     }
     catch(err){
         console.log(err.message);
