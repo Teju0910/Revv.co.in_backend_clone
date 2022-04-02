26e73253b98b400b71727271d580d49d7372a0da
Teju0910 <tejasvini_fw15_007@masai.school>
2022-04-02 10:06:28 +0530

    Sign-In,Login and Ouath, authenticate,,authorise

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -17,13 +17,13 @@ router.get("" , async (req,res) => {
 })
 
 
-router.post("/", async (req, res) => {
-    try {
-      const user = await User.create(req.body);  
-      return res.status(201).send(user);
-    } catch (err) {
-      return res.status(500).send({ message: err.message });
-    }
-  });
+// router.post("/", async (req, res) => {
+//     try {
+//       const user = await User.create(req.body);  
+//       return res.status(201).send(user);
+//     } catch (err) {
+//       return res.status(500).send({ message: err.message });
+//     }
+//   });
 
   module.exports = router;
